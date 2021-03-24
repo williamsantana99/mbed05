50916e0021e730d301b3ce16f3147342e51b070c
williamsantana99 <william.santana.allen@gmail.com>
2021-03-24 03:51:47 -0700

    add main.cpp

---
+++ 5_4_uLCD_demo/main.cpp
@@ -0,0 +1,45 @@
+// uLCD-144-G2 basic text demo program for uLCD-4GL LCD driver library
+
+//
+
+#include "mbed.h"
+
+#include "uLCD_4DGL.h"
+
+
+uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+
+
+int main()
+
+{
+
+    // basic printf demo = 16 by 18 characters on screen
+
+    
+    uLCD.reset();
+    uLCD.background_color(0xFFFFFF);
+    uLCD.textbackground_color(0xFFFFFF);
+
+    uLCD.cls();
+    uLCD.color(BLUE);
+    uLCD.printf("107061122\n"); //Default Green on black text
+   
+   
+    uLCD.text_width(4); //4X size text
+
+    uLCD.text_height(4);
+    
+    uLCD.color(GREEN);
+
+    for (int i=30; i>=0; --i) {
+
+        uLCD.locate(1,2);
+
+        uLCD.printf("%2d",i);
+
+        ThisThread::sleep_for(500ms);
+
+    }
+
+}
